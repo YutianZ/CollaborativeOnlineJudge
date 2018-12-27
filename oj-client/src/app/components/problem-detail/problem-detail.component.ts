a7b2a4a2d455689495c98c09432697faffa71a95
YutianZ <yutianz615@gmail.com>
2018-12-27 16:43:40 -0700

    new problem complete

--- oj-client/src/app/components/problem-detail/problem-detail.component.ts
+++ oj-client/src/app/components/problem-detail/problem-detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Params } from '@angular/router';
+import { DataService } from '../../services/data.service';
+import { Problem } from '../../models/problem.model';
 @Component({
   selector: 'app-problem-detail',
   templateUrl: './problem-detail.component.html',
   styleUrls: ['./problem-detail.component.css']
 })
 export class ProblemDetailComponent implements OnInit {
+  problem: Problem;
 
-  constructor() { }
+  constructor(private dataService: DataService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.problem = this.dataService.getProblem(+params['id']);
+    })
   }
-
 }
