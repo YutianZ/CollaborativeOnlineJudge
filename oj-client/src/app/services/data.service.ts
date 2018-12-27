dacb6c64b14f168cbbb33edf3f2c0689358c8772
YutianZ <yutianz615@gmail.com>
2018-12-27 15:41:47 -0700

    mock-problems and data service complete

---
+++ oj-client/src/app/services/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Problem } from "../models/problem.model";
+import { PROBLEMS } from "../mock-problems";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  //list of problems
+  problems: Problem[] = PROBLEMS;
+
+  constructor() { }
+
+  //return a list of problems
+  getProblems(): Problem[] {
+    return this.problems;
+  }
+  //return a problem by id
+  getProblem(id: number): Problem {
+    return this.problems.find((problem) => problem.id == id);
+  }
+}
